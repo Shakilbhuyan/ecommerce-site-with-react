f54a9b9e99a3161472a6c45a5fa817be80606666
Shakilbhuyan <sbhuiyan2956@gmail.com>
2023-03-30 23:16:37 +0600

    retrive product from stored cart

--- src/Componants/Shop/Shop.jsx
+++ src/Componants/Shop/Shop.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { addToDb, getShoppingCart } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css';
@@ -13,9 +14,29 @@ const Shop = () => {
             .then(data => setProducts(data));
     }, []);
 
-    const handleAddToCart = (product) =>{
+    useEffect(() => {
+        const storedCart = getShoppingCart();
+        const savedCart = [];
+        // Step 1 : get id
+        for (const id in storedCart) {
+            // step 2: find the product using id
+            const addedProduct = products.find(product => product.id === id);
+            if (addedProduct) {
+                // step 3: get quantity of the product
+                const quantity = storedCart[id];
+                addedProduct.quantity = quantity;
+                // step 4 : add the added cart to save cart
+                savedCart.push(addedProduct);
+            }
+            // step 5 : set the cart
+          setCart(savedCart);
+        }
+    }, [products])
+
+    const handleAddToCart = (product) => {
         const newCart = [...cart, product];
         setCart(newCart);
+        addToDb(product.id)
     };
 
     return (
@@ -25,7 +46,7 @@ const Shop = () => {
                     products.map(product => <Product
                         key={product.id}
                         product={product}
-                        handleAddToCart ={ handleAddToCart}
+                        handleAddToCart={handleAddToCart}
                     ></Product>)
                 }
             </div>
